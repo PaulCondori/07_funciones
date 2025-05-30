6a3ef7a0023a8343e9d8c7f502851be4c985ed51
PaulCondori <pscondoric@unjbg.edu.pe>
2025-05-29 21:35:12 -0500

    Agregar funcion: primera solucion ejercicio 6

---
+++ funcion6.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+bool esPrimo(int n) {
+    if (n <= 1) return false;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+
+int main() {
+    int numero;
+    cout << "Ingrese un numero entero positivo: ";
+    cin >> numero;
+
+    if (esPrimo(numero))
+        cout << numero << " es un numero primo." << endl;
+    else
+        cout << numero << " no es un numero primo." << endl;
+
+    return 0;
+}
+
