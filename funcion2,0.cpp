4bfa0b253394124e7d6440469be1074301c0b91a
PaulCondori <pscondoric@unjbg.edu.pe>
2025-05-30 21:40:09 -0500

    Actualización de función 2: solución mejorada

---
+++ funcion2,0.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+int obtenerCociente(int a, int b) {
+    int mayor = (a > b) ? a : b;
+    int menor = (a > b) ? b : a;
+
+    int cociente = 0;
+    while (mayor >= menor) {
+        mayor -= menor;
+        cociente++;
+    }
+    return cociente;
+}
+
+int obtenerResto(int a, int b) {
+    int mayor = (a > b) ? a : b;
+    int menor = (a > b) ? b : a;
+
+    while (mayor >= menor) {
+        mayor -= menor;
+    }
+    return mayor;
+}
+
+int main() {
+    int num1, num2;
+    cout << "Ingrese dos numeros enteros positivos: " << endl;
+    cin >> num1 >> num2;
+
+    if (num1 <= 0 || num2 <= 0) {
+    	cout << "--------------------------------" << endl;
+        cout << "Error: los numeros deben ser positivos." << endl;
+        return 1;
+    }
+
+    if (num1 == num2) {
+    	cout << "--------------------------------" << endl;
+        cout << "Error: los numeros no deben ser iguales." << endl;
+        return 1;
+    }
+
+    int cociente = obtenerCociente(num1, num2);
+    int resto = obtenerResto(num1, num2);
+    cout << "--------------------------------" << endl;
+    cout << "Cociente: " << cociente << endl;
+    cout << "Resto: " << resto << endl;
+
+    return 0;
+}
+
+
